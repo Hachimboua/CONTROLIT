b04111e60d7d3891a08fbf9ecd76991c951570f8
Hachimboua <hachimbouabdillah0423@gmail.com>
2025-05-21 10:21:26 +0100

    editing thedocs

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,16 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, r'C:\Users\houci\Documents\ENSAM\3eme annee\S6\Computer Vision\hand-gesture-recognition-mediapipe-main')
+
+
+
+
+
+
+
 
 project = 'Controlit'
 copyright = '2025, Hachimboua'
@@ -14,7 +24,13 @@ release = 'today'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',   # For NumPy/Google-style docstrings
+    'sphinx.ext.viewcode'
+]
+
+html_theme = 'sphinx_rtd_theme'
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
